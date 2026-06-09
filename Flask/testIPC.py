fd10287f0c4e73dbd5270a74d4aad62b3025e6de
Chris Lirakis <clirakis@gmail.com>
2026-06-08 21:17:47 -0400

    more debug

--- Flask/testIPC.py
+++ Flask/testIPC.py
@@ -8,7 +8,7 @@ def main():
     
     for i in range(20):
          pR0.Read()
-         print("index ", i, " value: ", pR0.R0)
+         print("index ", i, " time ", pR0.LastUpdate_Time_tv_sec," value: ", pR0.R0)
          
 
 if __name__ == "__main__":
