5ef59c59bb2cbd56fed1637cb90e17216894fa7d
Chris Lirakis <clirakis@gmail.com>
2026-06-09 10:37:10 -0400

    hunting bug in decode

--- Flask/testIPC.py
+++ Flask/testIPC.py
@@ -2,20 +2,27 @@ import sys
 import os
 import time
 from PySM.smWXT510 import smWXT510
+from WX.wxt510 import wxt510
 
 def main():
     pR0   = smWXT510()
+    pwxt  = wxt510()
+    logfile = open('debug.log','w') 
+    pwxt.SetLogfile(logfile)
+
     if(len(sys.argv)>1):
         if(sys.argv[1] == 'D'):
             pR0.debug = True
     
-    for i in range(20):
+    for i in range(5):
         if(pR0.Read()):
             print(' new data ')
         print("index ", i, " time ", pR0.LastUpdate_Time_tv_sec," value: ", pR0.R0)
+        pwxt.Decode(pR0.R0,logfile)
+
         time.sleep(1)
          
-         
+    logfile.close()
 
 if __name__ == "__main__":
     main()
