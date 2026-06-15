7609720dca6f675b755630a3139ea54612087e2a
Chris Lirakis <clirakis@gmail.com>
2026-06-15 08:28:01 -0400

    Debugging

--- Flask/testIPC.py
+++ Flask/testIPC.py
@@ -13,17 +13,18 @@ def main():
     if(len(sys.argv)>1):
         if(sys.argv[1] == 'D'):
             pR0.debug = True
-    
-    for i in range(5):
-        if(pR0.Read()):
-            print(' new data ')
-        val = pR0.R0
-        print("index ", i, " time ", pR0.LastUpdate_Time_tv_sec," value: ", val)
-        pwxt.Decode(val,logfile)
-        print ('Temp: ', pwxt.Temperature)
+    if (pR0.error == 0):
+        for i in range(5):
+            if(pR0.Read()):
+                print(' new data ')
+            val = pR0.R0
+            print("index ", i, " time ", pR0.LastUpdate_Time_tv_sec," value: ", val)
+            pwxt.Decode(val,logfile)
+            print ('Temp: ', pwxt.Temperature)
 
-        time.sleep(1)
-         
+            time.sleep(1)
+    else:
+        print('Could not attach to shared memory!')
     logfile.close()
 
 if __name__ == "__main__":
